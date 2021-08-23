3b4040d5e6ee2500d80a481903aa8327ae5da36e
rkongzy <sherlock_zy@hrbeu.edu.cn>
2021-08-23 16:51:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,5 @@
 sed -i 's/192.168.1.1/192.168.1.66/g' package/base-files/files/bin/config_generate
 
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
